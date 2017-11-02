c0061a50f5d600de3ee3ddc9f74d0fe89f03836f
Hays <stanfordhays@gmail.com>
2017-11-01 21:13:38 -0500

    Added email subscription form.

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+var Enquiry = keystone.list('Enquiry');
 
 exports = module.exports = function (req, res) {
 
@@ -8,6 +9,31 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+	locals.enquiryTypes = Enquiry.fields.enquiryType.ops;
+	locals.formData = req.body || {};
+	locals.validationErrors = {};
+	locals.enquirySubmitted = false;
+
+	// On POST requests, add the Enquiry item to the database
+	view.on('post', { action: 'contact' }, function (next) {
+
+		var newEnquiry = new Enquiry.model();
+		var updater = newEnquiry.getUpdateHandler(req);
+
+		updater.process(req.body, {
+			flashErrors: true,
+			fields: 'email',
+			errorMessage: 'There was a problem submitting your enquiry:',
+		}, function (err) {
+			if (err) {
+				locals.validationErrors = err.errors;
+			} else {
+				locals.enquirySubmitted = true;
+			}
+			next();
+		});
+	});
+
 
 	// Render the view
 	view.render('index');
